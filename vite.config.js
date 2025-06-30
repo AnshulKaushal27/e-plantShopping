c4354d07eb999107bb34047de61a31130b941576
AnshulKaushal27 <anshulkaushal27@gmail.com>
2025-06-30 04:27:23 -0400

    Your message describing the changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/shoppingreact",
   plugins: [react()],
-})
+})*/
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/e-plantShopping/', // 👈 Add this line
+});
